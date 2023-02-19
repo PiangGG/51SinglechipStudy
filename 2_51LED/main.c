f66943ced13de8058c7a522385de1611d78e9d4d
PiangGG <974805475@qq.com>
2023-02-19 19:04:23 +0800

    实现LED闪烁

--- 2_51LED/main.c
+++ 2_51LED/main.c
@@ -1,12 +1,24 @@
 #include "reg52.h"
 
+typedef unsigned char u8;
+typedef unsigned int u16;
+
 sbit LED1 = P2^0;
+
+//
+void Delay_10us(u16 ten_us)
+{
+	while(ten_us--);
+} 
 void main()
 {
-	LED1 = 0;
+	
 	
 	while(1)
 	{
-		
+		LED1 = 0;
+		Delay_10us(50000);
+		LED1 = 1;
+		Delay_10us(50000);
 	}
 }
